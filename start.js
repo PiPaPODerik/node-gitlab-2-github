877365ef7b2726e4ac19247e4d6a1a6df5082707
Jakesch, Patrik <Patrik.Jakesch+datev@DATEV.DE>
2025-02-10 12:19:35 +0100

    fixes start.js

--- start.js
+++ start.js
@@ -2,26 +2,27 @@ const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-const inputsOutputsDir = process.env.G2H_INPUTS_OUTPUTS_DIR || './inputs-outputs';
-const settingsFilePath = process.env.G2H_INPUTS_OUTPUTS_DIR ? path.join(inputsOutputsDir, './settings.ts') : path.join(__dirname, './settings.ts');
+const inputOutputVarValid = process.env.G2H_INPUTS_OUTPUTS_DIR !== undefined && process.env.G2H_INPUTS_OUTPUTS_DIR !== '' && process.env.G2H_INPUTS_OUTPUTS_DIR !== null;
+const inputsOutputsDir = inputOutputVarValid ? process.env.G2H_INPUTS_OUTPUTS_DIR : './inputs-outputs';
+const settingsFileSourcePath = path.join(inputsOutputsDir, './settings.ts');
 
 const envFile = path.resolve('.env');
 const nodeArgs = fs.existsSync(envFile) ? `--env-file=${envFile}` : '';
 
-if (!process.env.G2H_INPUTS_OUTPUTS_DIR && !nodeArgs.includes('G2H_INPUTS_OUTPUTS_DIR')) {
-  console.log(`Environment variable G2H_INPUTS_OUTPUTS_DIR is not set. Using default inputs-outputs dir ${settingsFilePath}.`);
+if (!inputOutputVarValid && !nodeArgs.includes('G2H_INPUTS_OUTPUTS_DIR')) {
+  console.log(`Environment variable G2H_INPUTS_OUTPUTS_DIR is not set. Using default inputs-outputs dir ${settingsFileSourcePath}.`);
 }
 
-if (fs.existsSync(settingsFilePath)) {
-  console.log(`Using settings file: ${settingsFilePath}`);
+if (fs.existsSync(settingsFileSourcePath)) {
+  console.log(`Using settings file: ${settingsFileSourcePath}`);
 } else {
-  throw new Error(`Settings file not found: ${settingsFilePath} ...Aborting. Please provide the path to settings.ts`);
+  throw new Error(`Settings file not found: ${settingsFileSourcePath} ...Aborting.`);
 }
 
-if (path.dirname(settingsFilePath) !== __dirname) {
-  const destinationPath = path.join(__dirname, path.basename(settingsFilePath));
-  fs.copyFileSync(settingsFilePath, destinationPath);
-  console.log(`Copied settings file from ${settingsFilePath} to: ${destinationPath}`);
+if (path.dirname(settingsFileSourcePath) !== __dirname) {
+  const destinationPath = path.join(__dirname, path.basename(settingsFileSourcePath));
+  fs.copyFileSync(settingsFileSourcePath, destinationPath);
+  console.log(`Copied settings file from ${settingsFileSourcePath} to: ${destinationPath}`);
 }
 
 const tsNodeRegister = '-r ts-node/register';
